39fe34505bc941880248f68fc60e167b389e9d82
Thinura-Nethz <thinuranethsara999@gmail.com>
2025-06-13 17:33:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
 }
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "LJM0EACb#k5FuqfZmW-TuFQc-GMJbOMXh8TYmMq5F08szlsWznBQ",
+  SESSION_ID: process.env.SESSION_ID || "7dMyxbhA#2vzGFrc4Qz4_k79eSDPT9ahPkhsFJeZ8H51xAhXEJj4",
   MONGODB: process.env.MONGODB || "mongodb://mongo:bIGDhAWAnCOZVPmNAFPdEPgZhAIEguCi@interchange.proxy.rlwy.net:43630",
   OWNER_NUM: process.env.OWNER_NUM || "94774571418",
   MODE: process.env.MODE || "public",

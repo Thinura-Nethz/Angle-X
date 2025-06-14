1b1a063771f8101fdb81720e6b779998fab4640e
Thinura-Nethz <thinuranethsara999@gmail.com>
2025-06-14 08:20:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
 }
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "LJM0EACb#k5FuqfZmW-TuFQc-GMJbOMXh8TYmMq5F08szlsWznBQ",
+  SESSION_ID: process.env.SESSION_ID || "6ZUy1D5R#rxVn7v9QpCCk0e0WzItxoSIVCgf-4kXdOIRtu-8Iass",
   MONGODB: process.env.MONGODB || "mongodb://mongo:bIGDhAWAnCOZVPmNAFPdEPgZhAIEguCi@interchange.proxy.rlwy.net:43630",
   OWNER_NUM: process.env.OWNER_NUM || "94774571418",
   MODE: process.env.MODE || "public",
